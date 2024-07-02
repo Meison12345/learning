153ac178e30db15271d571cff95970193ba9f9a7
Meison <69295331+Meison12345@users.noreply.github.com>
2024-07-02 11:49:19 +0300

    Add files via upload

---
+++ medium/footer.php
@@ -0,0 +1,7 @@
+<div class="center line-wrapper">
+    <div class="line"></div>
+</div>
+
+<footer class="footer center">
+    <p class="footer-text">© 2023 — 2412 «Галактический вестник»</p>
+</footer>
---
+++ medium/header.php
@@ -0,0 +1,9 @@
+<header class="center header">
+    <nav class="header-nav">
+        <a href="index.php">
+            <img src="/img/logo.svg" alt="Лого сайта" width="390" height="74">
+        </a>
+
+        <a href="/index.php">Главная</a>
+    </nav>
+</header>
